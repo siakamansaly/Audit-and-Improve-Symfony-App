48b24d621b7ad6ad33b4748db1911c83d09925ba
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-07-05 16:15:58 +0200

    update property user (nullable=true)

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -42,7 +42,7 @@ class Task
 
     /**
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="tasks")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(nullable=true)
      */
     private $user;
 
