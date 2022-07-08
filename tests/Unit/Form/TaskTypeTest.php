c56d5c19ee19def9c26be26d7c2b86889cefc31b
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-07-08 14:06:27 +0200

    Add unit test for forms

---
+++ tests/Unit/Form/TaskTypeTest.php
@@ -0,0 +1,46 @@
+<?php 
+namespace App\Tests\Unit\Form;
+
+use App\Form\TaskType;
+use App\Entity\Task;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class TaskTypeTest extends TypeTestCase
+{
+    public function testSubmitValidDataTaskForm()
+    {
+        $formData = [
+            'title' => 'test',
+            'content' => 'test2',
+        ];
+
+        $model = new Task();
+        $form = $this->factory->create(TaskType::class, $model);
+
+        $expected = new Task();
+        $expected->setTitle($formData['title']);
+        $expected->setContent($formData['content']);
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+
+        $this->assertEquals($expected->getTitle(), $model->getTitle());
+
+        $this->assertEquals($expected->getContent(), $model->getContent());
+    }
+
+    public function testCustomFormViewTaskForm()
+    {
+        $formData = new Task();
+
+        $formData->setTitle('test du jour');
+        $formData->setContent('content test');
+
+        $view = $this->factory->create(TaskType::class, $formData)->createView();
+
+        $this->assertSame('test du jour', $view->children['title']->vars['data']);
+        
+        $this->assertSame('content test', $view->children['content']->vars['data']);
+    }
+}
